d50d0ad4af46778b73b8cf1e3b505e8511c6e494
gabrielbmotta <gabrielbenmotta@gmail.com>
2022-05-19 15:21:17 -0400

    Add some comments, add README

---
+++ README.md
@@ -0,0 +1,18 @@
+## Arduino Analog Trigger
+
+### Usage:
+
+To create analog triggers, send commands to the serial port of the Arduino device, followed by a new line character (`\n`). As of now, the following commands are recognized:
+
+
+#### [number]
+
+ex.: 100
+
+Sends a trigger pulse of height [number].
+
+#### w[number]
+
+ex.: w500
+
+Sets the trigger pulse width to [number] milliseconds for all subsequent trigger pulses.
--- nk_trigger/nk_trigger.ino
+++ nk_trigger/nk_trigger.ino
@@ -11,21 +11,21 @@ int triggerWidth = 200;
 //====================================================================
 //====================================================================
 
-const int pin_13 = 13;
-bool newCommandAvailable = false;
-String serialCommand = "";
+const int pin_13 = 13;              // LED to indicate trigger sent.
+bool newCommandAvailable = false;   // Gloabl flag to track commands.
+String serialCommand = "";          // Buffer to store new commands.
 
 //====================================================================
-
+/*Setup outputs and their initial values;*/
 void setup() {
   pinMode(pin_13, OUTPUT);
   Serial.begin(9600);
   DDRD = B11111111;
-  PORTD = 0;
+  PORTD = defaultOuptut;
 }
 
 //====================================================================
-
+/*Wait for command and, when availbale, parse it.*/
 void loop() {
   if (newCommandAvailable) {
     parseCommand();
@@ -34,49 +34,45 @@ void loop() {
 }
 
 //====================================================================
-
+/*Parse command in global var 'serialCommand' and acts upon it.*/
 void parseCommand()
 {
   int commandValue = 0;
   int commandLength = serialCommand.length();
-
+  
+  /*We read commands back to front.*/
   for(int i = commandLength - 1; i >= 0; i--)
   {
     char charI = serialCommand.charAt(i);
+
+    /*If we have a number, add it to result accounting for magintude.*/
     if(charI >= '0' && charI <= '9')
     {
       commandValue += (charI - '0') * pow(10, (commandLength - 1) - i);
     }
+
+    /*If we have a 'w', adjust trigger width.*/
     else if(charI == 'w') 
     {
-      setWidth(commandValue);
+      triggerWidth = commandValue;
       return;
     } 
-    else {
+
+    /*If anything else shows up, give up and return early.*/
+    else 
+    {
       return;
     }
   }
+  
+  /*We get her if we did not adjust width or return early.*/
   triggerWithHeight(commandValue);
-  return;
 }
 
 //====================================================================
-
-void setWidth(int widthValue)
-{
-   triggerWidth = widthValue;
-}
-
-//====================================================================
-
+/*Output 'heightValue' for 'triggerWidth' milliseconds.*/
 void triggerWithHeight(int heightValue)
 {
-//  int initialTime = millis();
-//  while(millis() < initialTime + triggerWidth){
-//      PORTD = heightValue;
-//      digitalWrite(pin_13, HIGH);
-//  }
-
   PORTD = heightValue;
   digitalWrite(pin_13, HIGH);
   delay(triggerWidth);
@@ -85,7 +81,7 @@ void triggerWithHeight(int heightValue)
 }
 
 //====================================================================
-
+/*Resets the command global variables.*/
 void readyForNextCommand()
 {
   serialCommand = "";
@@ -93,12 +89,13 @@ void readyForNextCommand()
 }
 
 //====================================================================
-
+/*Reads from serial port to'serialCommand', with delimiter '\n'.*/
 void serialEvent()
 {
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    if (inChar == '\n') {
+    if (inChar == '\n') /*We never send '\n' to 'serialCommand'.*/  
+    { 
       newCommandAvailable = true;
       return;
     }
