6e9b9726b4a9a448e13047992e1676fcad6b3817
JuanGPC <juangpc@users.noreply.github.com>
2022-06-08 17:00:06 -0400

    update make output values max at 255

--- test_wave/test_wave.ino
+++ test_wave/test_wave.ino
@@ -1,6 +1,6 @@
 int value = 0;
-float value1 = 0;
-
+float value1 = 0.;
+float sampledValue = 0.0;
 void setup() {
   Serial.begin(9600);
   DDRD = B11111111;
@@ -9,14 +9,17 @@ void setup() {
 void loop() {
   for (int i = 0; i < 360; i++) {
     value1 = sin(i * DEG_TO_RAD) + 1;
-    value = ( value1 * 255.0) / 2;
-
+    value = ( value1 * 255.0) / 2.0;
+  
     PORTD = value;
-    delay(50);
-    Serial.print(value1);
-    Serial.print(",");
+    delay(5);
+//    Serial.print(value1);
+//    Serial.print(",");
+    sampledValue = analogRead(A0) * 255.0 / 1008.0;
+    
     Serial.print(value);
     Serial.print(",");
-    Serial.println(analogRead(A0));
+    Serial.println(sampledValue);
   }
 }
+
